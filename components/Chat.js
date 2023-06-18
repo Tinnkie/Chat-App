2fdec3ae25883776d73b301685390a28de14d32b
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-18 20:48:02 +0200

    import GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,48 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const { name, color } = route.params;
+    //create message state for GiftedChat
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         navigation.setOptions({ title: name });
+        //setting the state with a static message 
+        setMessages([
+          {
+            _id: 1,
+            text: "Hello developer",
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "React Native",
+              avatar: "https://placeimg.com/140/140/any",
+            },
+          },
+        ]);
+
       }, []);
 
+      const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages =>
+          GiftedChat.append(previousMessages, messages),
+        )
+      }, [])
+
  return (
-  <View style={[styles.container, { backgroundColor: color }]}>
-     <Text>Hello Screen2!</Text>
-   </View>
+  <View>
+  <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+    {/* Fixing the Android Keyboard */}
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+    </View>
  );
 }
 
