696582c82460de53ab7b03c677ed89a140bc746c
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-20 01:57:38 +0200

    import and render map view

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,7 @@ import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, query, orderBy, } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import CustomActions from "./CustomActions";
+import MapView from 'react-native-maps';
 
 const Chat = ({ route, navigation, db, isConnected, storage }) => {
     const { name, color, userID } = route.params;
@@ -92,6 +93,27 @@ const Chat = ({ route, navigation, db, isConnected, storage }) => {
         return <CustomActions storage={storage} {...props} />;
       };
 
+      const renderCustomView = (props) => {
+        const { currentMessage} = props;
+        if (currentMessage.location) {
+          return (
+              <MapView
+                style={{width: 150,
+                  height: 100,
+                  borderRadius: 13,
+                  margin: 3}}
+                region={{
+                  latitude: currentMessage.location.latitude,
+                  longitude: currentMessage.location.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+              />
+          );
+        }
+        return null;
+      }
+
  return (
   <View style={[styles.container, { backgroundColor: color }]}>
   <GiftedChat style={styles.textingBox}
@@ -100,6 +122,7 @@ const Chat = ({ route, navigation, db, isConnected, storage }) => {
       renderBubble={renderBubble}
       renderInputToolbar={renderInputToolbar}
       renderActions={renderCustomActions}
+      renderCustomView={renderCustomView}
       onSend={messages => onSend(messages)}
       user={{
         _id: userID,
