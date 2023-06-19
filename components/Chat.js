4b65a2219e1756a9327c3e08f0cb90b5c7b66bbf
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-19 22:28:33 +0200

    import AsyncStorage

--- components/Chat.js
+++ components/Chat.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, query, orderBy, } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Chat = ({ route, navigation, db }) => {
     const { name, color, userID } = route.params;
